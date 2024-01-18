96d5e26e44e30a08dfbcf35eb7f46da39981651a
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-18 17:27:55 +0530

    18th jan 4 get from Database

--- blog/main.py
+++ blog/main.py
@@ -17,7 +17,7 @@ def get_db():
 
 
 @app.post('/blog')
-def create_blog(request: schemas.Blog, db:Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body, price=request.price)
     db.add(new_blog)
     db.commit()
@@ -26,13 +26,25 @@ def create_blog(request: schemas.Blog, db:Session = Depends(get_db)):
 
 
 @app.post('/bank')
-def create_bank(bank: schemas.Bank, db:Session = Depends(get_db)):
+def create_bank(bank: schemas.Bank, db: Session = Depends(get_db)):
     new_bank = models.Bank(surname=bank.surname, name=bank.name, balance=bank.balance)
     db.add(new_bank)
     db.commit()
     db.refresh(new_bank)
     return new_bank
 
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blogs = db.query((models.Blog)).filter(models.Blog.id == id).first()
+    return blogs
+
+
+@app.get('/blog')
+def show(db: Session = Depends(get_db)):
+    blogs = db.query((models.Blog)).all()
+
+    return blogs
 
 if __name__ == '__main__':
     uvicorn.run(app)
