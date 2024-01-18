73769ee2fcebbe27491ec7fae294f7278d7d9aab
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-18 14:16:18 +0530

    18th jan 2 Pydantic Database connection

--- blog/main.py
+++ blog/main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
-from typing import Optional
-from . import schemas
+from .database import SessionLocal, engine
+from . import schemas, models
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.post('/blog')
 def create_blog(blog: schemas.First):
-    # return {f'Title of the blog is { blog.title} and price is {blog.price}'}
-    return blog
+    return {f'Title of the blog is { blog.title} and price is {blog.price}'}
 
-
-@app.get('/')
-def root(id: int):
-    return {'data': f'{id} is your personal ID'}
 
 
 if __name__ == '__main__':
