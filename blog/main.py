3e59a0c78a23e57c05c415eefcee07fed30cb3f9
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-19 11:04:56 +0530

    19 JAN , CRUD get,post,put,delete

--- blog/main.py
+++ blog/main.py
@@ -26,18 +26,36 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @app.post('/bank')
-def createbank(bank: schemas.Bank, db: Session = Depends(get_db)):
+def create_bank(bank: schemas.Bank, db: Session = Depends(get_db)):
     new_bank = models.Bank(surname=bank.surname, name=bank.name, balance=bank.balance*4)
     db.add(new_bank)
     db.commit()
     db.refresh(new_bank)
     return new_bank
 
-@app.delete('/blog/{id}', status_code=200)
-def destroy(id,db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session = False)
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} is not found')
+
+    blog.delete(synchronize_session = False)
     db.commit()
     return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not found")
+    blog.update(request.model_dump())
+    db.commit()
+    return 'Updated'
+
 
 @app.get('/blog/{id}', status_code=200)
 def show(id, response:Response, db: Session = Depends(get_db)):
